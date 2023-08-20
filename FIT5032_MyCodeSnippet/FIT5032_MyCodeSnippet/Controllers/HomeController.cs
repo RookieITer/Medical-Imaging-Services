13baaea1667a855a851a682060d76417d1f0f635
RookieITer <jduu0036@student.monash.edu>
2023-08-20 16:24:18 +1000

    update weekly prac

--- FIT5032_MyCodeSnippet/FIT5032_MyCodeSnippet/Controllers/HomeController.cs
+++ FIT5032_MyCodeSnippet/FIT5032_MyCodeSnippet/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FIT5032_MyCodeSnippet.Models.Hello_World;
 
 namespace FIT5032_MyCodeSnippet.Controllers
 {
@@ -15,7 +16,13 @@ namespace FIT5032_MyCodeSnippet.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            //ViewBag.Message = "Your application description page.";
+
+            // create instance of the Hello object
+            Hello hello = new Hello();
+
+            // assigned the ViewBag.Message to the result
+            ViewBag.Message = hello.GetHello();
 
             return View();
         }
