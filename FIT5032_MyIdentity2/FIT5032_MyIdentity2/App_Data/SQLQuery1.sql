f5ea4290b459520cd1bf98becede8679d68effc6
RookieITer <jduu0036@student.monash.edu>
2023-10-08 19:53:20 +1100

    forgot to push changes

---
+++ FIT5032_MyIdentity2/FIT5032_MyIdentity2/App_Data/SQLQuery1.sql
@@ -0,0 +1,20 @@
+﻿-- Create table 'Students'
+CREATE TABLE [dbo].[Students] (
+	[Id] INT IDENTITY (1,1) NOT NULL,
+	[FirstName] nvarchar (max) NOT NULL,
+	[LastName] nvarchar (max) NOT NULL,
+		[UserID] nvarchar (max) NOT NULL
+	PRIMARY KEY (Id)
+);
+GO
+
+-- Create table 'Units'
+CREATE TABLE [dbo].[Units] (
+	[Id] INT IDENTITY (1,1) NOT NULL,
+	[Name] nvarchar (max) NOT NULL,
+	[Description] nvarchar (max) NOT NULL,
+		[StudentId] int NOT NULL
+	PRIMARY KEY (Id),
+	FOREIGN KEY (StudentId) REFERENCES Students (Id)
+);
+GO
---
+++ FIT5032_MyIdentity2/FIT5032_MyIdentity2/App_Data/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Students] (
+	[Id] INT IDENTITY (1,1) NOT NULL,
+	[FirstName] nvarchar (max) NOT NULL,
+	[LastName] nvarchar (max) NOT NULL,
+		[UserID] nvarchar (max) NOT NULL
+	PRIMARY KEY (Id)
+);
+GO
